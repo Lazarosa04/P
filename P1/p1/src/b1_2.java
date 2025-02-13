f21d567d425ba148c5ab144cfae0f6231cd0948d
Lazarosa04 <lazarosa@ua.pt>
2025-02-13 14:07:55 +0000

    exc2 bem feito

--- P1/p1/src/b1_2.java
+++ P1/p1/src/b1_2.java
@@ -1,31 +1,66 @@
-
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.Dictionary;
-import java.util.Hashtable;
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class b1_2 {
-    public static void main(String[] args) throws IOException {
 
-        Dictionary<String, Double> dic = new Hashtable<>();
-    
-        Scanner scan = new Scanner(System.in);
-        //read in the line from stdin, if it's empty, then break out OR process it
-        while (scan.hasNextLine()){ 
-            String line = scan.nextLine();
-            String [] elements = line.split(" ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        HashMap<String, Double> hashmap = new HashMap<String, Double>();
 
+        while (sc.hasNextLine()){
+            String a = sc.nextLine();       // input
+
+            String[] elements = a.split(" ");   // separar elementos num array
+
+            // Caso seja uma atribuição e dps do "=" ter uma expressão
+            // Fazer a expressão a direita e igualar o resultado à variavel
+            if (elements[1].equals("=")  && elements.length > 3){
+                double temp = calculo(elements[2], elements[3], elements[4], hashmap);
+                System.out.println(temp);
+                hashmap.put(elements[0], temp);
+            }
+            // Caso seja uma atribuição, igualar o valor à direita à variavel
+            else if (elements[1].equals("=")){
+                double temp = Double.parseDouble(elements[2]);
+                System.out.println(temp);
+                hashmap.put(elements[0], temp);
+            }
+            // Não havendo atribuição, faz-se a expressão
+            else if (!elements[1].equals("=")){
+                System.out.println(calculo(elements[0], elements[1], elements[2], hashmap));
+            }
+            else{
+                System.err.println("Invalid Operator");
+            }
         }
     }
 
+    public static double calculo(String op1, String operador, String op2, HashMap<String, Double> hashmap){
+        if (hashmap.containsKey(op1)){
+            op1 = hashmap.get(op1).toString();
+        }
+        if (hashmap.containsKey(op2)){
+            op2 = hashmap.get(op2).toString();
+        }
 
-    public static boolean isNumeric(String str) { 
-        try {  
-          Double.parseDouble(str);  
-          return true;
-        } catch(NumberFormatException e){  
-          return false;  
-        }  
-      }
+        double n = Double.parseDouble(op1);
+        double  m = Double.parseDouble(op2);
+        
+        switch(operador) {
+            case "+":
+                return n + m;
+            case "-":
+                return n - m;
+            case "*":
+                return n * m;
+            case "/":
+                return n / m;
+            default:
+                System.err.println("Error: Operation failed");
+            }
+        return 0;
+    
+    }
+      
 }
