b84c9b3d2b829a7d19d9d7b55df8c6d08a836175
Lazarosa04 <lazarosa@ua.pt>
2025-03-20 10:29:06 +0000

    coisa

--- P2/bloco2/b2_03/Calculator.g4
+++ P2/bloco2/b2_03/Calculator.g4
@@ -1,18 +1,20 @@
 grammar Calculator;
 
 program:
-    stat* EOF
-    ;
+    stat * EOF;
+
 stat:
-    expr? NEWLINE
-    ;
+    expr ? NEWLINE;
+
 expr:
-    expr op=('*'|'/'|'%') expr  #ExprMulDivMod
-    | expr op=('+'|'-') expr    #ExprAddSub
-    | Integer                   #EOF
-    | '(' expr ')'              #ExprParent
+     op = ('+' | '-') expr                  # ExprUnary
+    |   expr op = ('*' | '/' | '%') expr    # ExprMultDivMod
+    |   expr op = ('+' | '-') expr          # ExprAddSub
+    |   Integer                             # ExprInteger
+    | '(' expr ')'                          # ExprParent
     ;
-Integer: [0-9]+ ;
-NEWLINE: '\r'? '\n' ;
-WS: [ \t]+ -> skip ;
-COMMENT: '#' .*? '\n' -> skip ;
+
+Integer: [0-9]+;
+NEWLINE: '\r' ? '\n';
+WS: [ \t]+ -> skip;
+COMMENT: '#' .*? '\n' -> skip;
--- P2/bloco2/b2_03/Execute.java
+++ P2/bloco2/b2_03/Execute.java
@@ -1,38 +1,64 @@
+
 @SuppressWarnings("CheckReturnValue")
-public class Execute extends CalculatorBaseVisitor<Double> {
+public class Execute extends CalculatorBaseVisitor<Integer> {
 
-   @Override public Double visitProgram(CalculatorParser.ProgramContext ctx) {
-      Double res = null;
+   @Override public Integer visitProgram(CalculatorParser.ProgramContext ctx) {
+      Integer res = null;
       return visitChildren(ctx);
       //return res;
    }
 
-   @Override public Double visitStat(CalculatorParser.StatContext ctx) {
-      Double res = null;
+   @Override public Integer visitStat(CalculatorParser.StatContext ctx) {
+      Integer res = null;
+      if (ctx.expr() != null) {
+         res = visit(ctx.expr());
+         if (res != null) {
+            System.out.println(res);
+         }
+      }
+      return res;
+   }
+
+   @Override public Integer visitExprAddSub(CalculatorParser.ExprAddSubContext ctx) {
+      Integer res = null;
+      Integer e1 = visit(ctx.expr(0));
+      Integer e2 = visit(ctx.expr(1));
+      String op = ctx.op.getText();
+      if (e1 != null && e2 != null) {
+         switch (op) {
+            case "+":
+               res = e1 + e2;
+               break;
+            case "-":
+               res = e1 - e2;
+               break;
+            default:
+               break;
+         }
+      }
+      return res;
+   }
+
+   @Override public Integer visitExprParent(CalculatorParser.ExprParentContext ctx) {
+      Integer res = null;
       return visitChildren(ctx);
       //return res;
    }
 
-   @Override public Double visitExprMulDivMod(CalculatorParser.ExprMulDivModContext ctx) {
-      Double res = null;
+   @Override public Integer visitExprUnary(CalculatorParser.ExprUnaryContext ctx) {
+      Integer res = null;
       return visitChildren(ctx);
       //return res;
    }
 
-   @Override public Double visitExprAddSub(CalculatorParser.ExprAddSubContext ctx) {
-      Double res = null;
+   @Override public Integer visitExprInteger(CalculatorParser.ExprIntegerContext ctx) {
+      Integer res = null;
       return visitChildren(ctx);
       //return res;
    }
 
-   @Override public Double visitExprParent(CalculatorParser.ExprParentContext ctx) {
-      Double res = null;
-      return visitChildren(ctx);
-      //return res;
-   }
-
-   @Override public Double visitEOF(CalculatorParser.EOFContext ctx) {
-      Double res = null;
+   @Override public Integer visitExprMultDivMod(CalculatorParser.ExprMultDivModContext ctx) {
+      Integer res = null;
       return visitChildren(ctx);
       //return res;
    }
