e14713a9be4f3714c4d3af99a71fb927a5d5b6e3
Lazarosa04 <lazarosa@ua.pt>
2025-03-27 11:53:36 +0000

    main do 5

--- P2/bloco2/b2_05/NumbersMain.java
+++ P2/bloco2/b2_05/NumbersMain.java
@@ -1,12 +1,14 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class NumbersMain {
-   public static HashMap<String,String> readNumbers() {
+   public static HashMap<String, String> readNumbers() {
       try {
          // create a CharStream that reads from the numbers.txt file:
          CharStream input = CharStreams.fromStream(new FileInputStream("numbers.txt"));
@@ -27,16 +29,38 @@ public class NumbersMain {
             ParseTreeWalker walker = new ParseTreeWalker();
             Read listener0 = new Read();
             walker.walk(listener0, tree);
-            return listener0.output();
+            HashMap<String, String> result = listener0.output();
+
+      //      Print the HashMap
+      //      for (Map.Entry<String, String> entry : result.entrySet()) {
+      //         System.out.println(entry.getKey() + " -> " + entry.getValue());
+      //      }
+
+            return result;
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      } catch (RecognitionException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+      return null;
+   }
+
+   public static void main(String[] args) {
+      HashMap<String, String> dictionary = readNumbers();
+      Scanner scan = new Scanner(System.in);  // Create a Scanner object
+      String text = scan.nextLine();       // input
+      String[] elements = text.split("[,\\s\\.\\-\\+\\*\\^\\/\\!]");   // tenta separar tudo para evitar casos como o twenty-four
+      for(String s : elements){
+         if(dictionary.containsKey(s.trim())){
+            System.out.print(dictionary.get(s) + " ");
+         }
+         else {
+            System.out.print(s + " ");
          }
       }
-      catch(IOException e) {
-         e.printStackTrace();
-         System.exit(1);
-      }
-      catch(RecognitionException e) {
-         e.printStackTrace();
-         System.exit(1);
-      }
+      scan.close();
    }
 }
